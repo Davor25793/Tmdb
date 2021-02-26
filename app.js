687b4e4f5ecae660f25dde106e7e71f7cfb8904a
Davor25793 <roko257993@gmail.com>
2021-02-26 22:39:17 +0100

    changes

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const DETAILS = 'https://api.themoviedb.org/3/movie/3?api_key=dd132df044d85760fd
 const CAST = 'https://api.themoviedb.org/3/movie/3/credits?api_key=dd132df044d85760fdd79f3192642f6a&language=en-US'
 
 const moviesContainer = document.querySelector('.movies-container')
+const singleMovie = document.querySelector('.single-movie')
+const casts = document.querySelector('.cast');
 
 
 async function getMovies(url){
@@ -25,6 +27,7 @@ getMovies(APIURL)
 
 //SHOW MOVIES
 function showMovies(data){
+  moviesContainer.innerHTML = '' 
   let output = '';
   console.log(data)
 
@@ -59,12 +62,52 @@ function getButtons(){
       }
 
       getSingleMovie(id)
-        .then(data => console.log(data))
+        .then(data => {
+          displaySingleMovie(data)
+          displayCast(id)
+        })
         .catch(err => console.log(err))
     })
   })
 }
 
+//DISPLAY CAST
+async function displayCast(id){
+  const response = await fetch(`https://api.themoviedb.org/3/movie/${id}/credits?api_key=dd132df044d85760fdd79f3192642f6a&language=en-US`)
+  const result = await response.json();
+  const cast = result.cast;
+  console.log(cast)
+
+  let output = ''
+
+  cast.map(cast => {
+    output += `
+    <img src=${IMGPATH + cast.profile_path} alt="">
+    <h3>${cast.character}</h3>
+    <h3>${cast.name}</h3>
+    `
+  })
+  casts.innerHTML = output;
+}
+
+
+
+//Display single movie 
+function displaySingleMovie(data){
+  console.log(data)
+  const{overview, backdrop_path} = data;
+
+  moviesContainer.innerHTML = '';
+
+  let output = `
+    <img src=${IMGPATH + backdrop_path} alt="">
+    <div class="content">
+      <h2>Summary</h2>
+      <p>${overview}</p>
+    </div>
+  `
+ singleMovie.innerHTML = output;
+}
 
 
 
