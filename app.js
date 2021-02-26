d34fe2bf880d5d8b70dd435b361bdadc4f2ba6d9
Davor25793 <roko257993@gmail.com>
2021-02-26 09:52:09 +0100

    changes

--- app.js
+++ app.js
@@ -1,15 +1,31 @@
 const APIURL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=dd132df044d85760fdd79f3192642f6a'
 
 const IMGPATH = 'https://image.tmdb.org/t/p/w1280'
+const SEARCHAPI = 'https://api.themoviedb.org/3/search/movie?api_key=dd132df044d85760fdd79f3192642f6a&query='
 
 const moviesContainer = document.querySelector('.movies-container');
 
-async function getMovies(){
-  const response = await fetch(APIURL)
-  const data = await response.json();
-  console.log(data);
 
-  data.results.forEach(movie => {
+const search = document.querySelector('#search')
+
+async function getMovies(url){
+  const response = await fetch(url)
+  const data = await response.json();
+  console.log(data)
+
+  showMovies(data.results);
+
+} 
+
+getMovies(APIURL);
+
+
+function showMovies(movies){
+
+  moviesContainer.innerHTML = ''
+
+  movies.forEach(movie => {
+
 
     const {poster_path, title, vote_average} = movie;
 
@@ -23,11 +39,22 @@ async function getMovies(){
     </div>
     `
 
+
    moviesContainer.appendChild(movieEl)
 
   })
+}
 
-  return data
-} 
+const form = document.querySelector('#form');
 
-getMovies();
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+ const searchTerm = search.value;
+
+  if(searchTerm){
+    getMovies(SEARCHAPI + searchTerm)
+    search.value = ''
+  }
+})
+
